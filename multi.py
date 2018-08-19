aa38dd7e925eae0ee3fc33fbb94cfae00c2f7761
Meowcolm024 <luoyicong1026@live.com>
2018-08-19 13:23:35 +0800

    Add files via upload

--- multi.py
+++ multi.py
@@ -1,81 +1,99 @@
-import os
 import cv2
 import time
-
-
-def tap(x0, y0):
-    cmdTap = 'adb shell input tap {x1} {y1}'.format(
-        x1=x0,
-        y1=y0
-    )
-    print(cmdTap)
-    os.system(cmdTap)
-
-
-def swipe(a0, b0, a1, b1, delay0):
-    cmdSwipe = 'adb shell input swipe {a2} {b2} {a3} {b3} {delay1}'.format(
-        a2=a0,
-        b2=b0,
-        a3=a1,
-        b3=b1,
-        delay1=delay0
-    )
-    print(cmdSwipe)
-    os.system(cmdSwipe)
+import random
+import lib.ats
+import numpy as np
 
 
 def sr(sh):
+    xSR = random.randrange(800, 1200)
+    ySR = random.randrange(400, 800)
+
     imgSR = cv2.imread(sh, 0)
-    templateSR = cv2.imread('SR.png', 0)
+    templateSR = cv2.imread('res/SR.png', 0)
 
     resSR = cv2.matchTemplate(imgSR, templateSR, cv2.TM_CCOEFF_NORMED)
     thresholdSR = 0.30
     if (resSR >= thresholdSR).any():
-        tap(1000, 600)
+        lib.ats.tap(xSR, ySR)
 
 
 def ssr(sh):
+    xSSR = random.randrange(800, 1200)
+    ySSR = random.randrange(400, 800)
+
     imgSSR = cv2.imread(sh, 0)
-    templateSSR = cv2.imread('SR.png', 0)
+    templateSSR = cv2.imread('res/SR.png', 0)
 
     resSSR = cv2.matchTemplate(imgSSR, templateSSR, cv2.TM_CCOEFF_NORMED)
     thresholdSSR = 0.30
     if (resSSR >= thresholdSSR).any():
-        tap(1000, 600)
+        lib.ats.tap(xSSR, ySSR)
 
 
 def end(sh):
     imgEND = cv2.imread(sh, 0)
-    templateEND = cv2.imread('END.png', 0)
+    templateEND = cv2.imread('res/END.png', 0)
 
     resEND = cv2.matchTemplate(imgEND, templateEND, cv2.TM_CCOEFF_NORMED)
     thresholdEND = 0.40
     if (resEND >= thresholdEND).any():
-        return 'e'
+        return 0
     else:
-        return 'c'
+        return 1
 
 
-def screenshot():
-    os.system('adb shell screencap -p /sdcard/sh.png')
-    os.system('adb pull /sdcard/sh.png .')
-    return "sh.png"
+def start(sh):
+    imgSTART = cv2.imread(sh, 0)
+    templateSTART = cv2.imread('res/START.png', 0)
+
+    resSTART = cv2.matchTemplate(imgSTART, templateSTART, cv2.TM_CCOEFF_NORMED)
+    thresholdSTART = 0.85
+    pos = []
+
+    if (resSTART >= thresholdSTART).any():
+        loc = np.where(resSTART >= thresholdSTART)
+        for pt in zip(*loc[::-1]):
+            pos.append(pt)
+        return pos
+    else:
+        return 0
 
 
 def main():
-    tap(900, 900)
-    swipe(700, 800, 1500, 250, 1000)
-    time.sleep(5)
+    delay = random.randrange(1, 5)
+    gap = random.uniform(0.5, 1.5)
+    x1 = random.randrange(600, 800)
+    y1 = random.randrange(600, 800)
+    x2 = random.randrange(1300, 1500)
+    y2 = random.randrange(200, 400)
+    dly = random.randrange(500, 1000)
 
-    while screenshot():
-        time.sleep(1)
-        sh = screenshot()
-        if end(sh) == 'c':
-            ssr(sh)
-            sr(sh)
-        elif end(sh) == 'e':
-            tap(1000, 600)
-            break
+    sh = lib.ats.screenshot()
+    pos = start(sh)
+
+    if pos != 0:
+        lib.ats.tap(pos[0][0], pos[0][1])
+        lib.ats.swipe(x1, y1, x2, y2, dly)
+
+        time.sleep(delay)
+
+        while lib.ats.screenshot():
+            time.sleep(gap)
+
+            sh = lib.ats.screenshot()
+
+            if end(sh) == 1:
+                ssr(sh)
+                sr(sh)
+            elif end(sh) == 0:
+                xEND = random.randrange(800, 1200)
+                yEND = random.randrange(400, 800)
+                lib.ats.tap(xEND, yEND)
+                print("Finished")
+                break
+    else:
+        print("Error")
 
 
 main()
--- single.py
+++ single.py
@@ -1,30 +1,9 @@
-import os
-
-
-def tap(x0, y0):
-    cmdTap = 'adb shell input tap {x1} {y1}'.format(
-        x1=x0,
-        y1=y0
-    )
-    print(cmdTap)
-    os.system(cmdTap)
-
-
-def swipe(a0, b0, a1, b1, delay0):
-    cmdSwipe = 'adb shell input swipe {a2} {b2} {a3} {b3} {delay1}'.format(
-        a2=a0,
-        b2=b0,
-        a3=a1,
-        b3=b1,
-        delay1=delay0
-    )
-    print(cmdSwipe)
-    os.system(cmdSwipe)
+import lib.ats
 
 
 def main():
-    tap(900, 900)
-    swipe(700, 800, 1500, 300, 1000)
+    lib.ats.tap(900, 900)
+    lib.ats.swipe(700, 800, 1500, 300, 1000)
 
 
 main()
